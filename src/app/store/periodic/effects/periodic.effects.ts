31e7c290c1e4d2919b9cc5728df43d9a85f877fc
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-05-20 11:00:31 +0300

    [ngrx] return action

--- src/app/store/periodic/effects/periodic.effects.ts
+++ src/app/store/periodic/effects/periodic.effects.ts
@@ -2,13 +2,13 @@ import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {PeriodicService} from '../../../services/periodic.service';
 import {addPeriodicAll, EPeriodicActionsType} from '../actions/periodic.actions';
-import {map, mergeMap} from 'rxjs/operators';
+import {exhaustMap, map} from 'rxjs/operators';
 
 @Injectable()
 export class PeriodicEffects {
 	loadPeriodic$ = createEffect(() => this.actions$.pipe(
 		ofType(EPeriodicActionsType.getAll),
-		mergeMap(() => this.periodicService.getPeriodicParentList().pipe(
+		exhaustMap(() => this.periodicService.getPeriodicParentList().pipe(
 			map(items => addPeriodicAll({items}))
 			// map(items => ({type: EPeriodicActionsType.addAll, items: [...items]}))
 		))
